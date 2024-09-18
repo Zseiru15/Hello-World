1cc53a1e95dcdc3de4698d86174fb911e6f15b86
Zseiru15 <co4786696@gmail.com>
2024-09-18 17:26:00 -0500

    modificacion de ejerciciofor

--- ejerciciofor.py
+++ ejerciciofor.py
@@ -24,17 +24,73 @@
 # print("------------------------------------------------")
 
 import time
-def ejercicio4 ():
-    cantidad1=int(input("ingrese su año de nacimiento: "))
-    cantidad2=int(input("ingrese el presente año: "))
-    cantidad3=cantidad2-cantidad1
-    print("la edad es: ", cantidad3)
-    for i in range(cantidad3+1):
-        print ("los años son:", cantidad1+i)
-        time.sleep(2)
-        print(i)
-    return
-ejercicio4 ()
+# def ejercicio4 ():
+#     cantidad1=int(input("ingrese su año de nacimiento: "))
+#     cantidad2=int(input("ingrese el presente año: "))
+#     cantidad3=cantidad2-cantidad1
+#     print("la edad es: ", cantidad3)
+#     for i in range(cantidad3+1):
+#         print ("los años son:", cantidad1+i)
+#         time.sleep(2)
+#         print(i)
+#     return
+# ejercicio4 ()
 
 # print("------------------------------------------------")
 
+# def numeros_impares():
+#     numero = int(input("ingrese el numero: "))
+#     for i in range(1, numero+1, 1):
+#         time.sleep(1)
+#         print(i, end=", ")
+#         if i == 15:
+#             break;
+# numeros_impares()
+
+# print("------------------------------------------------")
+
+# def reloj_seg():
+#     tiempo = int(input("ingrese el tiempo: "))
+#     for i in range(1, 60+1, 1):
+#         time.sleep(1)
+#         print("tiempo: ",i)
+#         if i == tiempo:
+#             break;
+#     print ("\33[101m" + "tiempo terminado" + "\33[0m")
+# reloj_seg()
+
+# print("------------------------------------------------")
+
+# def reloj_seg2():
+#     tiempo = int(input("ingrese el tiempo: "))
+#     for i in range(1, 60+1, 1):
+#         time.sleep(1)
+#         print("tiempo: ",i)
+#         if i == tiempo:
+#             break;
+#     print ("\33[101m" + "tiempo terminado" + "\33[0m")
+# reloj_seg2()
+
+# print("------------------------------------------------")
+
+# def interes():
+#     cantidad=int(input("ingrese la cantidad: "))
+#     interes_anual=int(input("ingrese el interes anual: "))
+#     tiempo=int(input("ingrese el tiempo de inversion: "))
+#     valor=cantidad
+#     for i in range(tiempo):
+#         calculo1=(valor*interes_anual)/100
+#         calculo2=valor+calculo1
+#         valor=calculo2
+#         time.sleep(1)
+#         print("el año ",i+1," ",valor)
+#     print ("\33[4m" + "las ganancias en ",tiempo," años son: ",valor," " + "\33[0m")
+# interes()
+
+# print("------------------------------------------------")
+
+def numeros():
+    cantidad=int(input("ingrese la cantidad de asteriscos: "))
+    for i in range(cantidad+1):
+        print("*" * i)
+numeros()
